c735a727a9173a8f98af40d51e80ad9c4276153a
Bhavya Srinivas <127350976+BhavyaSrinivas2003@users.noreply.github.com>
2023-08-04 18:49:44 +0530

    Update chatBot.js

--- chatBot.js
+++ chatBot.js
@@ -2,25 +2,28 @@ function chatbotFun() {
     let chatbotMsgList = ["Hi", "Hey", "good things take time", "enjoy the moment", "be in the moment", "Good Morning", "What's Up", "Good Evening", "u r beautiful the way ur", "How can I help you?", "Thank You"];
     let chatContainer = document.getElementById("chatContainer");
     let chatMsgListLength = chatbotMsgList.length;
+}
 
-    function sendMsgBtn() {
-        let userInput = document.getElementById("userInput");
-        let userInputValue = userInput.value;
-        let userInputContainerDiv = document.createElement("div");
-        userInputContainerDiv.classList.add("msg-to-chatbot-container");
-        let userInputContainer = document.createElement("span");
-        userInputContainer.classList.add("msg-to-chatbot");
-        userInputContainer.textContent = userInputValue;
-        userInput.value = "";
-        userInputContainerDiv.appendChild(userInputContainer);
-        chatContainer.appendChild(userInputContainerDiv);
-        let count = Math.ceil(Math.random() * chatMsgListLength - 1);
-        let chatInputContainerDiv = document.createElement("div");
-        chatInputContainerDiv.classList.add("msg-from-chatbot-container");
-        let chatInputContainer = document.createElement("span");
-        chatInputContainer.textContent = chatbotMsgList[count];
-        chatInputContainer.classList.add("msg-from-chatbot");
-        chatInputContainerDiv.appendChild(chatInputContainer);
-        chatContainer.appendChild(chatInputContainerDiv);
-    }
+function sendMsgBtn() {
+    let chatbotMsgList = ["Hi", "Hey", "good things take time", "enjoy the moment", "be in the moment", "Good Morning", "What's Up", "Good Evening", "u r beautiful the way ur", "How can I help you?", "Thank You"];
+    let chatContainer = document.getElementById("chatContainer");
+    let chatMsgListLength = chatbotMsgList.length;
+    let userInput = document.getElementById("userInput");
+    let userInputValue = userInput.value;
+    let userInputContainerDiv = document.createElement("div");
+    userInputContainerDiv.classList.add("msg-to-chatbot-container");
+    let userInputContainer = document.createElement("span");
+    userInputContainer.classList.add("msg-to-chatbot");
+    userInputContainer.textContent = userInputValue;
+    userInput.value = "";
+    userInputContainerDiv.appendChild(userInputContainer);
+    chatContainer.appendChild(userInputContainerDiv);
+    let count = Math.ceil(Math.random() * chatMsgListLength - 1);
+    let chatInputContainerDiv = document.createElement("div");
+    chatInputContainerDiv.classList.add("msg-from-chatbot-container");
+    let chatInputContainer = document.createElement("span");
+    chatInputContainer.textContent = chatbotMsgList[count];
+    chatInputContainer.classList.add("msg-from-chatbot");
+    chatInputContainerDiv.appendChild(chatInputContainer);
+    chatContainer.appendChild(chatInputContainerDiv);
 }
